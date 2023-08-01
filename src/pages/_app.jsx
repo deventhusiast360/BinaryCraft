89043f5340558065e2e199edbea9b7c099e756a9
Pritam Mondal <deventhusiast360@gmail.com>
2023-08-01 14:23:38 +0530

    updated _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,15 @@
-import '@/styles/globals.css'
-import { Josefin_Sans } from 'next/font/google'
+import "@/styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+import Head from "next/head";
 
-const JS = Josefin_Sans({ weight: '400',subsets: ['latin'] })
+const JS = Josefin_Sans({ weight: "400", subsets: ["latin"] });
 export default function App({ Component, pageProps }) {
-  return (<main className={JS.className}>
-    <Component {...pageProps} />
-      </main>
-    )
+  return (
+    <main className={JS.className}>
+      <Head>
+        <title>BinaryCraft</title>
+      </Head>
+      <Component {...pageProps} />
+    </main>
+  );
 }
