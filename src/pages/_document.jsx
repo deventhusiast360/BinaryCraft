b87963b63de69569f87426267f3c3f7b1d4a1c25
Pritam Mondal <deventhusiast360@gmail.com>
2023-08-01 14:43:17 +0530

    updated package.json

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -5,7 +5,6 @@ export default function Document() {
     <Html lang="en">
       <Head >
         <link rel='shortcut icon' href='/favicon.ico'/>
-        <title>BinaryCraft</title>
       </Head>
       <body>
         <Main />
